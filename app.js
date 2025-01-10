6c5f780575bef44d358341e92d26bd51bc7577a0
Rabiya Memon <rabiaghulam56@gmail.com>
2025-01-10 12:41:20 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+ const hidden_text=document.getElementById("hidden_text");
+const read_more=document.getElementById("read_more");
+read_more.onclick=()=>{
+if(hidden_text.style.display==='none'){
+    hidden_text.style.display='inline';
+    read_more.textContent='Read less'
+}
+else{
+    hidden_text.style.display='none';
+    read_more.textContent ='Read More' ;
+}};
+
+const hidden_text1=document.getElementById("hidden_text1");
+const read_more1=document.getElementById("read_more1");
+read_more1.onclick=()=>{
+if(hidden_text1.style.display==='none'){
+    hidden_text1.style.display='inline';
+    read_more1.textContent='Read less'
+}
+else{
+    hidden_text1.style.display='none';
+    read_more1.textContent='Read More' ;
+}};
+ 
+const hidden_text2=document.getElementById("hidden_text2");
+const read_more2=document.getElementById("read_more2");
+read_more2.onclick=()=>{
+if(hidden_text2.style.display==='none'){
+    hidden_text2.style.display='inline';
+    read_more2.textContent='Read less'
+}
+else{2
+    hidden_text2.style.display='none';
+    read_more2.textContent='Read More' ;
+}};
+
+const hidden_text3=document.getElementById("hidden_text3");
+const read_more3=document.getElementById("read_more3");
+read_more3.onclick=()=>{
+if(hidden_text3.style.display==='none'){
+    hidden_text3.style.display='inline';
+    read_more3.textContent='Read less'
+}
+else{
+    hidden_text3.style.display='none';
+    read_more3.textContent='Read More' ;
+}};
+
